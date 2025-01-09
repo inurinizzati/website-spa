d0605a7ea24c1d68e5a87a650d3f9d2c45ffc7b1
Nurin Izzati <117788773+inurinizzati@users.noreply.github.com>
2025-01-09 23:09:46 +0800

    Add files via upload

---
+++ src/helpers/ServiceList.js
@@ -0,0 +1,39 @@
+import Massages from "../media/massage.jpg";
+import FacialTreatments from "../media/facial.webp";
+import SaunaRoom from "../media/saunaroom(1).jpeg";
+import NailCare from "../media/nailcare.jpg";
+import HairRemoval from "../media/Underarm-laser-hair-removal-HD-picture.jpg";
+import MedicalSpaServices from "../media/BOTOX-e1601743955473.jpg";
+
+export const ServiceList = [
+  {
+    name: "Massages",
+    image: Massages,
+    price: 15.99,
+  },
+  {
+    name: "Facial Treatments",
+    image: FacialTreatments,
+    price: 11.99,
+  },
+  {
+    name: "Sauna Room",
+    image: SaunaRoom,
+    price: 256.53,
+  },
+  {
+    name: "Nail Care",
+    image: NailCare,
+    price: 17.99,
+  },
+  {
+    name: "Hair Removal",
+    image: HairRemoval,
+    price: 4.99,
+  },
+  {
+    name: "Medical Spa Services",
+    image: MedicalSpaServices,
+    price: 1997.99,
+  },
+];
